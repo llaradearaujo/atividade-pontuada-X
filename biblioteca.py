583f53eda08de80ebe959981de900d2e2b15c17d
jamile8 <reisjamilesouza@gmail.com>
2024-10-24 19:55:27 +0000

    salve

--- biblioteca.py
+++ biblioteca.py
@@ -7,3 +7,10 @@ os.system ("cls || clear")
 #funcoes: adicionar livro, excluir livro, adicionar funcinario, adicionar leitor, excluir leitor
 
 
+print("======= BIBLIOTECA =======")
+
+
+def adicionar_livros(livro):
+    livros_adicionados = []
+    livros_adicionados.append(livro)
+    return livros_adicionados
